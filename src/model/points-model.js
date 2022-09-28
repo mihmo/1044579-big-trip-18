7306122bd4906ba85d8fde3241a1bfec3058491c
Mykhailo Movchan <mihmov@gmail.com>
2022-09-28 12:03:58 +0100

    обновил синтаксис

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -2,7 +2,9 @@ import { generatePoint } from '../mock/point';
 import { POINTLIST_ITEMS_NUMBER } from '../mock/setup.js';
 
 export default class PointsModel {
-  points = Array.from({length: POINTLIST_ITEMS_NUMBER}, generatePoint);
+  #points = Array.from({length: POINTLIST_ITEMS_NUMBER}, generatePoint);
 
-  get = () => this.points;
+  get points() {
+    return this.#points;
+  }
 }
--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -6,33 +6,44 @@ import PointItemView from '../view/point-item-view';
 import {render} from '../render.js';
 
 export default class TripPresenter {
+  #eventsContainer = null;
+  #pointsModel = null;
+  #offersModel = null;
+  #destinationsModel = null;
+
   eventContainerComponent = new EventsContainerView();
 
   init = (eventsContainer, pointsModel, offersModel, destinationsModel) => {
-    this.eventsContainer = eventsContainer;
-    this.pointsModel = pointsModel;
-    this.offersModel = offersModel;
-    this.destinationsModel = destinationsModel;
+    this.#eventsContainer = eventsContainer;
+    this.#pointsModel = pointsModel;
+    this.#offersModel = offersModel;
+    this.#destinationsModel = destinationsModel;
 
-    this.eventsPoints = [...this.pointsModel.get()];
+    this.eventsPoints = [...this.#pointsModel.points];
 
-    render(new SortView(), this.eventsContainer);
-    render(this.eventContainerComponent, this.eventsContainer);
-    render(new PointItemView(this.eventsPoints[0]), this.eventContainerComponent.getElement());
+    render(new SortView(), this.#eventsContainer);
+    render(this.eventContainerComponent, this.#eventsContainer);
+    render(new PointItemView(this.eventsPoints[0]), this.eventContainerComponent.element);
     // console.log('Point Offers: ' + this.eventsPoints[0].offers); // временно оставил для дебага.
-
-    const offers = [...this.offersModel.get(this.eventsPoints[0])];
+    const tst = [...this.#offersModel.offers];
+    console.log(tst);
+    console.log(this.#offersModel.offers);
+    console.log(this.#offersModel);
+    // console.log(this.#offersModel.offers(this.eventsPoints[0].offers));
+    console.log(this.eventsPoints[0].offers);
+    // const offers = [...this.#offersModel.points(this.eventsPoints[0])];
+    const offers = [...this.#offersModel.points(this.eventsPoints[0])];
     // console.log(this.eventsPoints[0].offers); // временно оставил для дебага.
     // console.log(this.offersModel.get()); // временно оставил для дебага.
-    const destinations = [...this.destinationsModel.get(this.eventsPoints[0])];
+    const destinations = [...this.#destinationsModel.get(this.eventsPoints[0])];
     // console.log(destinations[0]); // временно оставил для дебага.
-    render(new PointEditView(this.eventsPoints[0], offers, destinations), this.eventContainerComponent.getElement());
+    render(new PointEditView(this.eventsPoints[0], offers, destinations), this.eventContainerComponent.element);
 
     for (let i = 1; i < this.eventsPoints.length; i++) {
       render(new PointItemView(this.eventsPoints[i]), this.eventContainerComponent.getElement());
       // console.log(this.eventsPoints[i]); // временно оставил для дебага.
     }
 
-    render(new PointAddView(), this.eventContainerComponent.getElement());
+    render(new PointAddView(), this.eventContainerComponent.element);
   };
 }
